b4e9c2eb7aa52c44bc50d8d3ae7613fabf5f2198
Akashjayan1999 <akashjayan999@gmail.com>
2025-08-25 06:33:28 +0000

    passing data through middleware

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,80 @@
 use std::{clone, fmt::format, path, sync::Mutex};
 
-use actix_web::{    
-    get, post, web::{self, Json}, App, HttpResponse, HttpServer, Responder
+use actix_web::{
+    App, Error, HttpMessage, HttpRequest, HttpResponse, HttpServer, Responder,
+    body::MessageBody,
+    dev::{ServiceRequest, ServiceResponse},
+    get,
+    middleware::{Next, from_fn},
+    post,
+    web::{self, Json},
 };
+use serde::de;
 use serde_json::{self};
 
 #[actix_web::main]
 async fn main() {
-    let person =Person{
+    let person = Person {
         name: "Tom".to_string(),
         age: 18,
     };
-    let mut_pesron = web::Data::new(MutablePerson{
+    let mut_pesron = web::Data::new(MutablePerson {
         name: Mutex::new("Tom".to_string()),
         age: Mutex::new(18),
     });
-   HttpServer::new(move || {
-      App::new()
-          .app_data(web::Data::new(person.clone()))
-          .app_data(mut_pesron.clone())
-         .route("/", web::get().to(|| async { HttpResponse::Ok().body("Hello, world!") }))
-         // .route("/{name}", web::get().to(|name: web::Path<String>| async move {
-         //    HttpResponse::Ok().body(format!("Hello, {}!", name.into_inner()))
-         // }))
-         .route("/", web::post().to(|| async { HttpResponse::Ok().body("POST request!") }))
-         .route("/",web::delete().to(|| async { 
-            HttpResponse::Ok().body("DELETE request!") 
-         }))
-          .route("/",web::put().to(|| async { 
-            HttpResponse::Ok().body("PUT request!") 
-         })).service(hello)
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(person.clone()))
+            .app_data(mut_pesron.clone())
+            .route(
+                "/",
+                web::get().to(|| async { HttpResponse::Ok().body("Hello, world!") }),
+            )
+            // .route("/{name}", web::get().to(|name: web::Path<String>| async move {
+            //    HttpResponse::Ok().body(format!("Hello, {}!", name.into_inner()))
+            // }))
+            .route(
+                "/",
+                web::post().to(|| async { HttpResponse::Ok().body("POST request!") }),
+            )
+            .route(
+                "/",
+                web::delete().to(|| async { HttpResponse::Ok().body("DELETE request!") }),
+            )
+            .route(
+                "/",
+                web::put().to(|| async { HttpResponse::Ok().body("PUT request!") }),
+            )
+            .service(
+                web::scope("/api")
+                    .route("/hello2", web::get().to(hello2))
+                    .route("/world2", web::get().to(world2))
+                    .wrap(from_fn(my_middleware)),
+            )
+            .service(hello)
             .service(world)
             .service(dynamic)
             .service(user)
             .service(postuser)
             .service(hello3)
             .default_service(web::to(not_found))
-
-   })
-   .bind("127.0.0.1:8080").unwrap()
-   .run()
-   .await.unwrap();
+            .wrap(from_fn(my_middleware))
+    })
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
 }
 
 #[get("/hello")]
-async fn hello(person : web::Data<Person>)-> impl Responder {
+async fn hello(person: web::Data<Person>) -> impl Responder {
     let msg = format!("name: {}, age: {}", person.name, person.age);
     HttpResponse::Ok().body(msg)
 }
 
 #[get("/world")]
-async fn world(person: web::Data<MutablePerson>)-> impl Responder {
+async fn world(person: web::Data<MutablePerson>) -> impl Responder {
     let mut name = person.name.lock().unwrap();
     *name = "Tom".to_string();
     let mut age = person.age.lock().unwrap();
@@ -60,26 +84,25 @@ async fn world(person: web::Data<MutablePerson>)-> impl Responder {
 }
 
 #[get("/use/{id}")]
-async fn dynamic(path : web::Path<i32>)-> impl Responder {
+async fn dynamic(path: web::Path<i32>) -> impl Responder {
     HttpResponse::Ok().body(format!("Hello, {}!", path.into_inner()))
 }
 
-
 #[get("/user")]
-async fn user(info: web::Query<Info>)-> impl Responder {
-   let msg = format!("name: {}, age: {}", info.name, info.age);
+async fn user(info: web::Query<Info>) -> impl Responder {
+    let msg = format!("name: {}, age: {}", info.name, info.age);
     HttpResponse::Ok().body(msg)
 }
 
 #[post("/user")]
-async fn postuser(userItem: Json<User>)-> impl Responder {
-   let msg = format!("name: {}, age: {}", userItem.name, userItem.age);
+async fn postuser(userItem: Json<User>) -> impl Responder {
+    let msg = format!("name: {}, age: {}", userItem.name, userItem.age);
     HttpResponse::Ok().body(msg)
 }
 
 #[get("/hello3")]
-async fn hello3()-> impl Responder {
-    let person =Person{
+async fn hello3() -> impl Responder {
+    let person = Person {
         name: "Tom".to_string(),
         age: 18,
     };
@@ -87,39 +110,64 @@ async fn hello3()-> impl Responder {
 
     HttpResponse::Ok().json(person_json)
 }
-#[get("/hello2")]
-async fn hello2()-> impl Responder {
-    HttpResponse::BadRequest().body("Hello, world2!")
+
+async fn hello2(req: HttpRequest) -> impl Responder {
+    match req.extensions().get::<Person>() {
+        Some(msg) => HttpResponse::Ok().body(format!("name: {}, age: {}", msg.name, msg.age)),
+        None => HttpResponse::Ok().body("No data in extensions"),
+    }
+    // HttpResponse::BadRequest().body("Hello, world2!")
 }
 
-#[get("/world2")]
-async fn world2()-> impl Responder {
-    HttpResponse::InternalServerError().body("Hello, world2!")
+async fn world2(req: HttpRequest) -> impl Responder {
+    // HttpResponse::InternalServerError().body("Hello, world2!")
+    match req.extensions().get::<String>() {
+        Some(msg) => HttpResponse::Ok().body(msg.to_string()),
+        None => HttpResponse::Ok().body("No data in extensions"),
+    }
 }
-
 
 async fn not_found() -> impl Responder {
     HttpResponse::NotFound().body("404 Not Found")
 }
 
-#[derive(serde::Deserialize)]
-struct Info{
-      name: String,
-      age: u8,
+async fn my_middleware(
+    req: ServiceRequest,
+    next: Next<impl MessageBody>,
+) -> Result<ServiceResponse<impl MessageBody>, Error> {
+    println!("Hello from my middleware!");
+    //    req.extensions().insert("Some data");
+    let person = Person {
+        name: "Tom".to_string(),
+        age: 18,
+    };
+    req.extensions_mut()
+        .insert("Hello world from middleware".to_string());
+     req.extensions_mut()
+        .insert(person);
+    let res = next.call(req).await?;
+    Ok(res)
+    //    Ok(req.into_response(HttpResponse::Unauthorized().body("UnAuthrized")))
 }
 
 #[derive(serde::Deserialize)]
-struct User{
-      name: String,
-      age: u8,
-}
-#[derive(serde::Serialize,Clone)]
-struct Person{
+struct Info {
     name: String,
     age: u8,
 }
 
-struct MutablePerson{
-    name:Mutex<String>,
+#[derive(serde::Deserialize)]
+struct User {
+    name: String,
+    age: u8,
+}
+#[derive(serde::Serialize, Clone)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+struct MutablePerson {
+    name: Mutex<String>,
     age: Mutex<u8>,
 }
